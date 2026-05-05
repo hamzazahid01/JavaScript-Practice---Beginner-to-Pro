54cf3a2679e1164bc192a194b980b7a2bcc254c3
Hamza Zahid <hamzazahid0066example.com>
2026-05-05 12:23:21 +0500

    Promises Understanding

--- 12.Callbacks, Promises & Async Await/04.Promises.js
+++ 12.Callbacks, Promises & Async Await/04.Promises.js
@@ -1,11 +1,23 @@
-// basic promise
-let promise = new Promise((resolve, reject)=>{
-    let success = true;
-    if(success){
-        resolve("Done!!");
-    }else{
-        reject("Error!!");
-    }
+// // basic promise
+// let promise = new Promise((resolve, reject)=>{
+//     let success = true;
+//     if(success){
+//         resolve("Done!!");
+//     }else{
+//         reject("Error!!");
+//     }
+// });
+// promise.then((res)=>console.log(res) );
+// promise.catch((err)=>console.log(err));
+
+function fetchData(){
+    return promise = new Promise((resolve, reject)=>{
+        setTimeout(()=>{
+            resolve("Data Fetched");
+        },2000)
+    });
+}
+
+fetchData().then((res)=>{
+    console.log(res);
 });
-promise.then(()=>console.log("Success") );
-promise.catch(()=>console.log("Error"));
