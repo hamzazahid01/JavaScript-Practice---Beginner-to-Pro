a033dd1421a42c82a501be5343dfbc007c1b7299
Hamza Zahid <hamzazahid0066example.com>
2026-05-05 11:11:51 +0500

    Sync and Async in JavaScript

---
+++ 12.Callbacks, Promises & Async Await/Sync vs Async.js
@@ -0,0 +1,10 @@
+// sync line by line
+console.log("I am first line");
+console.log("I am seond line");
+console.log("I am third line");
+
+// async
+console.log("I am first line");
+ setTimeout(()=>{
+    console.log("I am second line");
+ },2000);
